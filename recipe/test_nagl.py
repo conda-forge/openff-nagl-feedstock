a56c31fd91b9282d5e24bcd9c819fd4d733d3f90
Matthew W. Thompson <mattwthompson@protonmail.com>
2025-10-29 15:00:10 -0500

    Bump maximum PyTorch to 2.8, improve test
    Typo
    
    MNT: Re-rendered with conda-smithy 3.52.3 and conda-forge-pinning 2025.10.29.19.20.1
    
    Actually bump PyTorch pin
    
    Do not change `package.name`
    
    Bump maximum PyTorch to 2.8, improve test
    
    Co-authored-by: Peter Bygrave <peterbygrave@users.noreply.github.com>

--- recipe/test_nagl.py
+++ recipe/test_nagl.py
@@ -1,6 +1,14 @@
 from openff.nagl import GNNModel
 from openff.nagl_models import list_available_nagl_models
 
+from openff.toolkit import Molecule
+
 assert len(list_available_nagl_models()) > 0
 
 [GNNModel.load(model) for model in list_available_nagl_models()]
+
+molecule = Molecule.from_smiles("CC(=O)OC1=CC=CC=C1C(=O)O")
+
+molecule.assign_partial_charges(
+    partial_charge_method=list_available_nagl_models()[-1],
+)
